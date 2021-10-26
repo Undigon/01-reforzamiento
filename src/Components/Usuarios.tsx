bce94e5f524f69a6c2064194c358feb4bc9501a7
Rodrigo Iván Avila Demitroff <rodrig1a@yahoo.com>
2021-10-26 17:35:58 -0300

    agregar botones next, prev.

--- src/Components/Usuarios.tsx
+++ src/Components/Usuarios.tsx
@@ -1,35 +1,10 @@
-import { useEffect, useRef, useState } from "react"
-import { idText } from "typescript";
-import { reqResApi } from "../api/reqRes"
-import { ReqResListado, Usuario } from '../interfaces/reqRes';
+
+import { Usuario } from '../interfaces/reqRes';
+import { useUsuarios } from '../hooks/useUsuarios';
 
 export const Usuarios = () => {
-
-    const [usuarios, setUsuarios] = useState<Usuario[]>([]);
-
-    const paginaRef = useRef(1);
-
-    useEffect(() => {
-        // llamado a la API.
-        cargarUsuarios();
-    }, [])
-
-    const cargarUsuarios = async() => {
-        const resp = await reqResApi.get<ReqResListado>('/users', {
-            params: {
-                page: paginaRef.current
-            }
-        })
-
-        if( resp.data.data.length > 0){
-            setUsuarios( resp.data.data );
-            paginaRef.current++;
-        }else{
-            alert('No hay mas registros');
-        }
-
-        setUsuarios( resp.data.data )
-    }
+    
+    const {usuarios, cargarUsuarios} = useUsuarios();
 
     const renderItem = ( usuario: Usuario) => {
         // Destructurando el objeto usuario.
---
+++ src/hooks/useUsuarios.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useRef, useState } from "react";
+import { reqResApi } from "../api/reqRes";
+import { ReqResListado, Usuario } from "../interfaces/reqRes";
+
+export const useUsuarios = () => {
+    const [usuarios, setUsuarios] = useState<Usuario[]>([]);
+
+    const paginaRef = useRef(1);
+
+    useEffect(() => {
+        // llamado a la API.
+        cargarUsuarios();
+    }, [])
+
+    const cargarUsuarios = async() => {
+        const resp = await reqResApi.get<ReqResListado>('/users', {
+            params: {
+                page: paginaRef.current
+            }
+        })
+
+        if( resp.data.data.length > 0){
+            setUsuarios( resp.data.data );
+            paginaRef.current++;
+        }else{
+            alert('No hay mas registros');
+        }
+    }
+
+    return {
+        usuarios,
+        cargarUsuarios
+    }
+}
