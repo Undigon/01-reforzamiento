21ab25f917bafcfb25005a1b2cdce5d9240f57f5
Matias Chicote <chicote.matias@gmail.com>
2021-10-26 11:36:42 -0300

    sección 12

--- src/typescript/TiposBasicos.tsx
+++ src/typescript/TiposBasicos.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
-
 export const TiposBasicos = () => {
+    
+    const nombre:string = 'Fernando';
+    const edad:number = 35;
+    const estaActivo:boolean = true;
+
+    const poderes:string[] = [];
+
+    poderes.push("1")
+    
     return (
         <>
-         <h3>Tipos básicos</h3>   
+         <h3>Tipos básicos</h3>
+         { nombre }, { edad }, { (estaActivo) ? 'activo' : 'no activo' }
+         <br/>
+         {poderes.join(', ')}
         </>
     )
 }
