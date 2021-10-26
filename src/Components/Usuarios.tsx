f1ab2401dd71be11890cc55ed08575afa4f0e69e
Rodrigo Iván Avila Demitroff <rodrig1a@yahoo.com>
2021-10-26 17:01:22 -0300

    secc. 22

--- src/Components/Usuarios.tsx
+++ src/Components/Usuarios.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import { idText } from "typescript";
 import { reqResApi } from "../api/reqRes"
 import { ReqResListado, Usuario } from '../interfaces/reqRes';
 
@@ -11,9 +12,27 @@ export const Usuarios = () => {
         reqResApi.get<ReqResListado>('/users')
             .then( resp => {
                 console.log(resp.data.data[0].last_name);
+                setUsuarios( resp.data.data )
             })
             .catch( console.log );
     }, [])
+
+    const renderItem = ( usuario: Usuario) => {
+        // Destructurando el objeto usuario.
+        const {avatar, first_name, last_name, email, id} = usuario
+        return (
+            <tr key={ id.toString() }>
+                <td><img src={avatar}
+                         alt={first_name +" avatar"}
+                         style={{
+                             width: 35,
+                             borderRadius: 100
+                         }} /></td>
+                <td>{first_name + " " + last_name}</td>
+                <td>{email}</td>
+            </tr>
+        )
+    }
 
     return (
         <>
@@ -26,7 +45,16 @@ export const Usuarios = () => {
                         <th>email</th>
                     </tr>
                 </thead>
+                <tbody>
+                    {
+                        usuarios.map( renderItem )
+                    }
+                </tbody>
             </table>
+            <button
+                className="btn btn-primary">
+                    Siguientes
+            </button>
         </>
     )
 }
